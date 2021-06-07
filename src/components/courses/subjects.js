f85a05d05a678801f078bb25ff9312c2b8d0d47f
Sandini Pitawala <67331586+Sandinipitawala@users.noreply.github.com>
2021-06-07 18:06:51 +0530

    Update subjects.js

--- src/components/courses/subjects.js
+++ src/components/courses/subjects.js
@@ -14,7 +14,7 @@ class Subjects extends Component {
   componentDidMount() {
     axios.get(`http://localhost:8099/course/${this.props.match.params.id}`)
     .then(response => { 
-      this.setState({subjects: response.data.subjects })
+      this.setState({subjects: response.data.data })
     })
     .catch(error =>{
       alert(error.message)
